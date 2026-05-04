db09377a59b37521fd9eeafb72dc7ebcb5b62ded
laidenspatrick <laidenspatrick@gmail.com>
2026-05-04 20:03:10 -0300

    Delete Jogos 2/GauchoGame directory

--- Jogos 2/GauchoGame/scripts/Fase1.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-## Fase 1 - Centro de Porto Alegre
-## Conecta sinais do Player ao HUD
-
-func _ready():
-	var player = $Player
-	var hud = $HUD
-	player.health_changed.connect(hud.update_health)
