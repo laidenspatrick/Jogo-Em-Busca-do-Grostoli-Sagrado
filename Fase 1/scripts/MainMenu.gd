16d832981fe04f6d9aa181329c8ec73b6fa14588
laidenspatrick <laidenspatrick@gmail.com>
2026-05-31 18:53:46 -0300

    Delete Fase 1 directory

--- Fase 1/scripts/MainMenu.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-## Menu Principal
-
-func _on_play_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/Fase1.tscn")
-
-func _on_quit_button_pressed():
-	get_tree().quit()
